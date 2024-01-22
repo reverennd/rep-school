4cc5eb1fe8aa12e13183461a92ca1483d1e96472
Alekssyla <alekssyla2@gmail.com>
2024-01-22 11:28:54 +0100

    complete project

---
+++ photo-fetcher/index.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import express from "express";
+
+const app = express()
+const  port = 3000
+
+app.use(express.static("public"));
+
+let numberImages = 4;
+let randomNumbers = []
+let grayScale = false
+
+async function generateNumbers() {
+    for (let i = 0; i < numberImages; i++) {
+        const randomNumber = Math.floor(Math.random * 200)
+        randomNumbers.push(randomNumber)
+    }
+}
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        numberImages: numberImages,
+        randomNumbers: randomNumbers,
+        grayScale: grayScale,
+    })
+})
+
+app.get("/new", (req, res) => {
+    numberImages += 4
+    res.redirect("/")
+})
+
+app.get("/gray", (req, res) => {
+    grayScale = !grayScale
+    res.redirect("/")
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
