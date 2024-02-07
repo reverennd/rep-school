a6bf8788371db6bb211c1edee4ecfdd89b26ef52
ArbjonMarasheviq <arbjonmarasheviq26@gmail.com>
2024-02-07 11:49:17 +0100

    react basic app

---
+++ react-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Images from './Images';
+import Button from './Button';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Images />
+      <Button />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ react-app/src/Images.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Images() {
+  return (
+    <div className='container'>
+        <img src='https://picsum.photos/id/237/200/300' alt='picsum'/>
+    </div>
+  )
+}
